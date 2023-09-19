cc7ac9bdd29f0e00aa67a2d9feb952e41d4c7d5e
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-09-19 13:57:49 -0300

    test: dollar multiplication

---
+++ src/dollar.spec.ts
@@ -0,0 +1,12 @@
+import { Dollar } from './dollar'
+
+
+
+describe('Dollar', () => {
+  test("should return 10 when five dollars are multiplied by two", () => {
+    const five = new Dollar(5)
+    five.times(2)
+    expect(five.amount).toEqual(10)
+  })
+
+})
