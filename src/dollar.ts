479ff0f9f5b8132eeb466436cf020acd4fde8335
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-09-19 13:57:28 -0300

    feat: add dollar class and multiplication

---
+++ src/dollar.ts
@@ -0,0 +1,14 @@
+
+
+
+
+export class Dollar {
+  amount: number
+  constructor(amount: number) {
+    this.amount = amount
+  }
+  times(multiplier: number): void {
+    this.amount *= multiplier
+  }
+
+}
